6475b35109c3eaf477bc0ec205799b17015f2829
Sarah R <phoesarah@hotmail.com>
2015-05-23 12:18:35 -0500

    update sql statements to create tables

--- SQLTABLECREATE.sql
+++ SQLTABLECREATE.sql
@@ -8,12 +8,19 @@ CREATE TABLE Flavors
  IngredientAmount VARCHAR(50) Foreign Key, WebLink VARCHAR(50));
 CREATE TABLE Food
 (FoodName VARCHAR(50)Primary Key, 
- Brand VARCHAR(100) Foreign Key, ProductClaim# int, FoodType VARCHAR(50), HealthRelated VARCHAR(100), FeedingGuidelines VARCHAR (50) );
+ Brand VARCHAR(100) Foreign Key, ProductClaim# int foreign key, FoodType VARCHAR(50), HealthRelated VARCHAR(100), FeedingGuidelines VARCHAR (50) );
 CREATE TABLE IngredientAmount 
-(Ingredient VARCHAR(50)Primary Key Foreign Key, Amount int(50) PRIMARY Key)
+(Ingredient VARCHAR(50)Primary Key Foreign Key, Amount int(50) PRIMARY Key);
 CREATE TABLE Ingredients 
-(Ingredient VARCHAR(50)Primary Key Foreign Key, Description VARCHAR(50))
+(Ingredient VARCHAR(50)Primary Key Foreign Key, Description VARCHAR(50));
 CREATE TABLE Manufacturer
-CREATE TABLE PetInformation
+(Manufacturer VARCHAR(50)Primary Key Foreign Key, Street VARCHAR(50)  City VARCHAR(50),
+  State VARCHAR(2), Zip  VARCHAR(13),  Country VARCHAR(50), Phone VARCHAR(20), Website VARCHAR(100), Logo VARCHAR(100));
+CREATE TABLE PetInformation 
+(PetName VARCHAR(50)Primary Key, Age int,  Activity VARCHAR(50),
+  Breed VARCHAR(50), Weight int, SpecialNeeds VARCHAR (50), FoodName VARCHAR(50) Foreign Key);
 CREATE TABLE ProductClaims
+(ProductClaim# int Primary Key, Claim VARCHAR(50));
 CREATE TABLE Veterinarian
+(Veterinarian int Primary Key, FirstName VARCHAR(50), LastName VARCHAR(50),  Street VARCHAR(50)  City VARCHAR(50),
+  State VARCHAR(2), Zip  VARCHAR(13),  Country VARCHAR(50), Phone VARCHAR(20), Website VARCHAR(100), Logo VARCHAR(100));
