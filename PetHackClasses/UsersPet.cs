4bd81a74a986a8cdfd8e2c59a3169a794439eb31
TylerSadler <tssadler88@gmail.com>
2015-05-23 08:32:27 -0500

    Add empty classes

---
+++ PetHackClasses/UsersPet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PetHackClasses
+{
+    public class UsersPet
+    {
+        public string Age { get; set; }
+        public string Weight { get; set; }
+        public string Activity { get; set; }
+        public string Breed { get; set; }
+        public string Name { get; set; }
+    }
+}
