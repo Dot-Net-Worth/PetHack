0cb37f160e5373b4413aa63d5ae534edf35c5343
TylerSadler <tssadler88@gmail.com>
2015-05-24 01:31:19 -0500

    Orginized files to make it easier to work with

---
+++ ASPPetHack/Compair.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASPPetHack
+{
+    class Compair
+    {
+        public static List<Products> bestMatches()
+        {
+            return new List<Products>();
+        }
+    }
+}
